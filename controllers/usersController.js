c7a30728c2d159eed4e1086ae8d16a900cf6ae90
Rupali Tanna <rupalirathod87@gmail.com>
2015-07-18 15:35:14 -0400

    login code

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,7 +4,52 @@ module.exports.controller = function(app) {
 	app.get('/login', function (req, res){
 		res.render('login');
 	});
+	
 	app.get('/signup', function (req, res){
 		res.render('signup');
+	});
+	
+	app.get('/', function(req, res) {
+    	if(!req.session.name) {
+        	res.redirect('/login');
+    	} else {
+        	res.send("Welcome back " + req.session.name);
+    	}
+	});
+
+	
+	app.post('/login', function(req, res) {
+    	bcrypt.hash(req.body.password, 10, function(err, hash) {
+        	var userObj = {
+            	username: req.body.name,
+            	password: hash
+        	};
+        	req.session.name = req.body.name;
+        	db.create('users', userObj, function(data) {
+            	res.redirect('/');
+        	})
+    	});
+	});
+
+	app.get('/compare', function(req, res) {
+    	res.render('home');
+	});
+
+	app.post('/compare', function(req, res) {
+    	//created ne findbyColumn for this
+    	db.findByColumn('users', 'username', req.body.username, function (data){
+        	//how you sign in 
+        	bcrypt.compare( req.body.password, data[0].password , function (err, result) {
+            	req.session.currentUser = req.body.name;
+            	res.send('youre logged in. happy reading.'); 
+        	});
+   		}); 
+    	res.redirect('/');
+	});
+
+	app.get('/logout', function(req, res) {
+  	// delete req.session.name;
+  		req.session.name = null;
+  		res.send('I have forgotten everything');
 	});
 }
