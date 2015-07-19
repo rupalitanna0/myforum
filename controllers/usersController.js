a633ff7ef0718bba333857d47871bd442a89f7a1
Rupali Tanna <rupalirathod87@gmail.com>
2015-07-19 09:42:51 -0400

    updated the login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,6 @@
 var db = require('../db.js');
 module.exports.controller = function(app) {
 //For all routes related to a post put them here
-	app.get('/login', function (req, res){
-		res.render('login');
-	});
-	
-	app.get('/signup', function (req, res){
-		res.render('signup');
-	});
 	
 	app.get('/', function(req, res) {
     	if(!req.session.name) {
