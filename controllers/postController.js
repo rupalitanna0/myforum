4965f82b369503bf06e258b80ec32bb2274a846f
Rupali Tanna <rupalirathod87@gmail.com>
2015-07-21 17:45:19 -0400

    m

--- controllers/postController.js
+++ controllers/postController.js
@@ -42,7 +42,7 @@ module.exports.controller = function(app) {
 			var voteObj={
 				votes: vote
 			}
-			res.render('/topics', vote);
+			res.render('/topics', voteObj);
 		});
 	});
 	
--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
-// var dbUrl = "pg://localhost/myforum_db";
-var dbUrl = process.env.DATABASE_URL
+var dbUrl = "pg://localhost/myforum_db";
+// var dbUrl = process.env.DATABASE_URL
 
 module.exports = {
   end: function() {
